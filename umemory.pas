dc9fd1e9f0e7ba1487c1f9f915b9b79c0e2cfdf9
erwan2212 <erwan2212@gmail.com>
2019-08-11 14:26:05 +0200

    sync

--- umemory.pas
+++ umemory.pas
@@ -9,9 +9,12 @@ uses
 
 function WriteMem(hprocess:thandle;offset:nativeint;bytes:array of byte):boolean;
 function ReadMem(hprocess:thandle;offset:nativeint;var bytes:array of byte):boolean;
+function SearchMem(hprocess:thandle;addr:pointer;sizeofimage:DWORD;pattern:array of byte):nativeint;
 
 
 implementation
+
+//type tbytes=array of byte;
 
 function WriteMem(hprocess:thandle;offset:nativeint;bytes:array of byte):boolean;
 var
@@ -29,6 +32,34 @@ fillchar(bytes,length(bytes),0);
 result:=ReadProcessMemory (hprocess,pointer(offset),@bytes[0],length(bytes),@read);
 //ideally should check read against length(bytes) as well...
 end;
+
+//https://github.com/gentilkiwi/mimikatz/blob/master/mimikatz/modules/kuhl_m_lsadump.c#L971
+//pattern should be a parameter to make this function generic...
+function SearchMem(hprocess:thandle;addr:pointer;sizeofimage:DWORD;pattern:array of byte):nativeint;
+var
+  i:nativeint;
+  buffer:array of byte;
+  read:cardinal;
+begin
+result:=0;
+setlength(buffer,length(pattern));
+//log('Searching...',0);
+  for i:=nativeint(addr) to nativeint(addr)+sizeofimage-length(buffer) do
+      begin
+      //fillchar(buffer,4,0);
+      if ReadProcessMemory( hprocess,pointer(i),@buffer[0],length(buffer),@read) then
+        begin
+        //log(inttohex(i,sizeof(pointer)));
+        if CompareMem (@pattern [0],@buffer[0],length(buffer)) then
+           begin
+           result:=i;
+           break;
+           end;
+        end;//if readprocessmemory...
+      end;//for
+//log('Done!',0);
+end;
+
 
 
 end.
