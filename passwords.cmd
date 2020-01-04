3a4976dca1620b3348b77d25b04ffadc728edd3c
erwan2212 <erwan2212@gmail.com>
2020-01-04 14:57:16 +0100

    sync

--- passwords.cmd
+++ passwords.cmd
@@ -1,14 +1,14 @@
 @echo off
 rem the below script will 
-rem go thru each line in passwords.txt
+rem go thru each line in passwords.lst
 rem call nthash-win64 /getntlmhash /input:line
 rem compare the output of nthash with provided parameter on the command line
 rem passwords.txt contains 25 most used passwords from 2011 to 2019 according to SplashData
 rem cheap bruteforce ... very slow ... ok for a few passwords ...
 setlocal ENABLEDELAYEDEXPANSION
 set input=%1
-rem for /F "usebackq tokens=*" %%A in ("passwords.txt") do echo %%A
-for /F "usebackq tokens=*" %%A in ("passwords.txt") do (
+rem for /F "usebackq tokens=*" %%A in ("passwords.lst") do echo %%A
+for /F "usebackq tokens=*" %%A in ("passwords.lst") do (
  rem %%A = a line in the file
  rem echo %%A
  set l=3
